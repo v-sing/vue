db8b3e30c5018bffbc463f31de1aa33106a027e2
v-sing <email1946357301@163.com>
2019-09-26 18:15:30 +0800

    是法人

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
+import $ from 'jquery'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import $ from 'jquery'
+
+window.$=$;
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import iview from '@/components/iview'
-import $ from 'jquery'
+
 
 Vue.use(Router)
 
